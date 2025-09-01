177453c30384cd75592e1d2ad94303a8f727d50d
Ricardo Casares <ricardocasares@gmail.com>
2025-09-01 12:33:41 +0200

    refactor: remove unused alert types and simplify alert module

--- src/UI/Alert.elm
+++ src/UI/Alert.elm
@@ -1,51 +1,25 @@
-module UI.Alert exposing (success, error, warning, info)
+module UI.Alert exposing (error)
 
 import Html exposing (Html, div)
 import Html.Attributes exposing (class)
 
 
 type AlertType
-    = Success
-    | Error
-    | Warning
-    | Info
+    = Error
 
 
 alert : AlertType -> List (Html.Attribute msg) -> List (Html msg) -> Html msg
 alert alertType attrs children =
     let
+        baseClass : String
         baseClass =
             case alertType of
-                Success ->
-                    "alert alert-success"
-
                 Error ->
                     "alert alert-error"
-
-                Warning ->
-                    "alert alert-warning"
-
-                Info ->
-                    "alert alert-info"
     in
     div (class baseClass :: attrs) children
-
-
-success : List (Html.Attribute msg) -> List (Html msg) -> Html msg
-success attrs children =
-    alert Success attrs children
 
 
 error : List (Html.Attribute msg) -> List (Html msg) -> Html msg
 error attrs children =
     alert Error attrs children
-
-
-warning : List (Html.Attribute msg) -> List (Html msg) -> Html msg
-warning attrs children =
-    alert Warning attrs children
-
-
-info : List (Html.Attribute msg) -> List (Html msg) -> Html msg
-info attrs children =
-    alert Info attrs children
